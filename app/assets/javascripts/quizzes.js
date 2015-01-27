d4cd003dbb99dfbe29672502e21052c735ee99d9
Peter Ching <peter.ching@gmail.com>
2015-01-26 23:06:23 -0500

    Prepare for end of quiz screen.

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -35,21 +35,6 @@ function scoring (timeTaken, attempts) {
 
 }
 
-	// Create a scoring function
-	// 	- Calculate raw base score as 100 / # of cards
-	// 	- Multiply base score by the following:
-	// 		- if 0 attempted, then * 1
-	// 		- if 1 attempted, then * 0.75
-	// 		- if 2 attempted, then * 0.5
-	// 		- if 3 or > attempts, then * 0
-	// 	Multiply resulting base score by attempts by the following:
-	// 		- if time taken = 0-10 seconds, then * 1
-	// 		- if time taken = 11-20 seconds, then * 0.9
-	// 		- if time taken = 21-30 seconds, then * 0.8
-	// 		- if time taken > 30 seconds, then * 0.75
-
-
-
 $(document).ready(function(){
 	$("button#startquiz").on("click", function(){
 		event.preventDefault();
@@ -81,30 +66,36 @@ $(document).ready(function(){
 	$(document).on("click", "button#answer", function(){
 		event.preventDefault();
 
-		attempt = $("#answer-text").val();
+		if (flashCards.length > 1) {
+			attempt = $("#answer-text").val();
 
-		$("#answer-text").val("");
+			$("#answer-text").val("");
 
-		if (attempt == flashCards[0].answer) {
+			if (attempt == flashCards[0].answer) {
 
-			var endTime = Date.now();
+				var endTime = Date.now();
 
-			var totalTimeInSeconds = (endTime - startTime)/1000;
+				var totalTimeInSeconds = (endTime - startTime)/1000;
 
-			// console.log(totalTimeInSeconds);
+				scores.push(scoring(totalTimeInSeconds));
 
-			scores.push(scoring(totalTimeInSeconds));
+				console.log(scores);
 
-			console.log(scores);
+				flashCards.shift();
+				renderQuizScreen();
+			} else if (numOfAttempts == 2) {
+				flashCards.shift();
+				renderQuizScreen();
+			} else {
+				numOfAttempts ++;
+			}
 
-			flashCards.shift();
-			renderQuizScreen();
-		} else if (numOfAttempts == 2) {
-			flashCards.shift();
-			renderQuizScreen();
+		console.log(flashCards.length);
+
 		} else {
-			numOfAttempts ++;
+			// render endQuiz
 		}
+
 
 	});
 
