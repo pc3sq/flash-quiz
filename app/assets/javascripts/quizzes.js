e490194535d583d7cea755a441a82b26b5c102fe
Peter Ching <peter.ching@gmail.com>
2015-01-26 22:27:52 -0500

    Jot down pseudocode for scoring algorithm.

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -8,6 +8,23 @@ function renderQuizScreen() {
 	$("#quiz").append('<br><input id="answer-text" type="text" placeholder="type answer here"><br><br><button id="answer">Submit Answer</button>');
 }
 
+function scoring () {
+
+	// Create a scoring function
+	// 	- Calculate raw base score as 100 / # of cards
+	// 	- Multiply base score by the following:
+	// 		- if 0 attempted, then * 1
+	// 		- if 1 attempted, then * 0.75
+	// 		- if 2 attempted, then * 0.5
+	// 		- if 3 or > attempts, then * 0
+	// 	Multiply resulting base score by attempts by the following:
+	// 		- if time taken = 0-10 seconds, then * 1
+	// 		- if time taken = 11-20 seconds, then * 0.9
+	// 		- if time taken = 21-30 seconds, then * 0.8
+	// 		- if time taken > 30 seconds, then * 0.75
+
+}
+
 
 $(document).ready(function(){
 	$("button#startquiz").on("click", function(){
