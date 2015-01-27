96e90771fd052cd8f3f65cf57db71b3a559f6b6c
Peter Ching <peter.ching@gmail.com>
2015-01-26 23:05:33 -0500

    Complete scoring algorithm.

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -1,15 +1,39 @@
 var quiz;
+var rawScorePerCard;
 var flashCards;
 var numOfAttempts = 0;
 var scores = []
+var startTime;
 
 function renderQuizScreen() {
 	$("#quiz").empty();
+	startTime = Date.now();
 	$("#quiz").append("<div>" + flashCards[0].question + "</div>");
 	$("#quiz").append('<br><input id="answer-text" type="text" placeholder="type answer here"><br><br><button id="answer">Submit Answer</button>');
 }
 
-function scoring () {
+function scoring (timeTaken, attempts) {
+	var score = rawScorePerCard
+
+	if (attempts == 1) {
+		score = score * 0.75;
+	} else if (attempts == 2) {
+		score = score * 0.5;
+	}
+
+	// requirements don't account for in between 10-11secs and 20-21secs
+
+	if (timeTaken > 10.0 && timeTaken < 20.0) {
+		score = score * 0.9;
+	} else if (timeTaken > 21.0 && timeTaken < 30.0) {
+		score = score * 0.8;
+	} else if (timeTaken > 30) {
+		score = score * 0.75
+	}
+
+	return score;
+
+}
 
 	// Create a scoring function
 	// 	- Calculate raw base score as 100 / # of cards
@@ -24,7 +48,6 @@ function scoring () {
 	// 		- if time taken = 21-30 seconds, then * 0.8
 	// 		- if time taken > 30 seconds, then * 0.75
 
-}
 
 
 $(document).ready(function(){
@@ -38,6 +61,7 @@ $(document).ready(function(){
     .done(function(response) {
       quiz = response.quiz;
       flashCards = response.flash_cards;
+      rawScorePerCard = 100 / flashCards.length;
 
       $("#topheading").text("Now take Quiz #" + quiz.id);
       $("#quiz").empty();
@@ -54,18 +78,25 @@ $(document).ready(function(){
     });
 	});
 
-
-// A pinto bean!
-
 	$(document).on("click", "button#answer", function(){
 		event.preventDefault();
-
 
 		attempt = $("#answer-text").val();
 
 		$("#answer-text").val("");
 
 		if (attempt == flashCards[0].answer) {
+
+			var endTime = Date.now();
+
+			var totalTimeInSeconds = (endTime - startTime)/1000;
+
+			// console.log(totalTimeInSeconds);
+
+			scores.push(scoring(totalTimeInSeconds));
+
+			console.log(scores);
+
 			flashCards.shift();
 			renderQuizScreen();
 		} else if (numOfAttempts == 2) {
