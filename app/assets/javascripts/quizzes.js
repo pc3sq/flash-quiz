c1e96610b18ad670210eb8099656e51cab5abe76
Peter Ching <peter.ching@gmail.com>
2015-01-26 19:24:09 -0500

    Prepare ajax to receive a new quiz object populated with flash cards; Trying newer jQuery ajax style with promises.

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -3,5 +3,26 @@ $(document).ready(function(){
 		event.preventDefault();
 		alert("clicked!");
 
-	})
-})
+		$.ajax({
+      url: 'http://www.google.com', // which route??? ex. quiz/new or quiz/create
+      type: 'POST', // type of http verb?
+      data: {}, // data sending, if any?
+      // success: function(response){
+      //   //what to do with the response?
+      // },
+      //  error: function(request, status, err){
+      //    // some error handling
+      //  }
+    })
+    .done(function() {
+      alert( "success" );
+    })
+    .fail(function() {
+      alert( "error" );
+    })
+    .always(function() {
+      alert( "complete" );
+    });
+
+	});
+});
